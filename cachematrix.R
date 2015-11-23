e5c4ba7cdaafd4b4ed709aca3d329accdda6a550
farid tayari <farid.tayari@gmail.com>
2015-11-22 22:55:15 -0500

    Farid tayari third try

--- cachematrix.R
+++ cachematrix.R
@@ -5,7 +5,7 @@ makeCacheMatrix <- function(x = matrix()) {
         set <- function(y) {
                 x <<- y
                 m <<- NULL
-        }
+        } 
         get <- function() x
         setInverse <- function(Solve) m <<- Solve
         getInverse <- function() m
@@ -13,7 +13,7 @@ makeCacheMatrix <- function(x = matrix()) {
              setInverse = setInverse,
              getInverse = getInverse)
 }
- 
+    
 ##This function checks to see if the invers is already calculated. 
 ##If it's already calculated returns the saved value and if not calculates 
 ##the invers of the matrix and saves the calculation.
